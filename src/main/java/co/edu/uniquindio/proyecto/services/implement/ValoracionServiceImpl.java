4b59ef8ce97503f6bb82caa96d262834ebde314f
Victor Manuel <victorma89@hotmail.com>
2024-09-18 13:54:54 -0500

    valoracionServiceImpl arreglos pendientes

--- src/main/java/co/edu/uniquindio/proyecto/services/implement/ValoracionServiceImpl.java
+++ src/main/java/co/edu/uniquindio/proyecto/services/implement/ValoracionServiceImpl.java
@@ -1,4 +1,41 @@
 package co.edu.uniquindio.proyecto.services.implement;
 
-public class ValoracionServiceImpl {
+import co.edu.uniquindio.proyecto.model.docs.Valoracion;
+import co.edu.uniquindio.proyecto.repositories.ValoracionRepo;
+import co.edu.uniquindio.proyecto.services.interfaces.ValoracionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class ValoracionServiceImpl implements ValoracionService {
+    private ValoracionRepo valoracionRepo;
+
+    public ValoracionServiceImpl(ValoracionRepo valoracionRepo){
+        this.valoracionRepo=valoracionRepo;
+    }
+
+    @Override
+    public List<Valoracion> listarValoracionByUsuario(String usuario) {
+        List<Valoracion> listarValoraciones =valoracionRepo.listarValoracionByUsuario(usuario);
+        return listarValoraciones;
+    }
+
+    @Override
+    public void crearValoracion(Valoracion valoracion) {
+        valoracionRepo.save(valoracion);
+    }
+
+    @Override
+    public List<Valoracion> obtenerValoracionByUsuarioID(String usuarioId) {
+        List<Valoracion> listaValoraciones =valoracionRepo.listarValoracionByUsuario(usuarioId);
+        return listaValoraciones;
+    }
+
+    @Override
+    public void eliminarValoracionByUsuarioId(String usuarioId, Valoracion valoracion) {
+        valoracionRepo.eliminarValoracionByUsuarioId(usuarioId,valoracion.getCodigo().toString());
+    }
 }
