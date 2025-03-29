b621896b2a136754ef5d56be5ad4ed15d5f9d049
CarlosMancera <125831393+CarlosMancera@users.noreply.github.com>
2025-03-29 17:30:47 -0500

    Update WebConfig.java

--- src/main/java/co/edu/uniquindio/proyecto/config/WebConfig.java
+++ src/main/java/co/edu/uniquindio/proyecto/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Aplica a todas las rutas
-                        .allowedOriginPatterns("*") // Permitir tu frontend
+                        .allowedOrigins("http://localhost:4200") // Permitir tu frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
